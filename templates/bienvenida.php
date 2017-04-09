86d2aed85d6fe392015ae90116f84daab14e4b06
Fernando Garcia <fkngv_10@hotmail.com>
2017-04-08 23:13:31 -0500

    Mejoras a Bienvenida y Logout

--- templates/bienvenida.php
+++ templates/bienvenida.php
@@ -8,10 +8,22 @@
         if (!$_SESSION) {
             header("Location:login.html");
         }
+        else {
+            include("conexion.php");
+            if ($_SESSION['tipo'] == 2) {
+                $obtener_nombre = "SELECT * FROM cliente WHERE rfc_cliente='$_SESSION[nombre]'";
+                $resultado = mysqli_query($conexion, $obtener_nombre);
+                $usuario = mysqli_fetch_assoc($resultado);
+                $nombre = $usuario['nombre_cliente'];
+            }
+            else {
+                $nombre = $_SESSION['nombre'];
+            }
+        }
     ?>
 </head>
 <body>
-    <h1><?php echo "Bienvenido $_SESSION[nombre]"; ?></h1>
+    <h1><?php echo "Bienvenido $nombre"; ?></h1>
     <br>
     <a href="logout.php">Cerrar sesión</a>
 </body>
--- templates/logout.php
+++ templates/logout.php
@@ -4,4 +4,7 @@
         session_destroy();
         header("Location:../index.html");
     }
+    else {
+        header("Location:login.html");
+    }
 ?>
