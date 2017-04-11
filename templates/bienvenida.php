2bf5528ca32ae7ab4242c7f690d955c773687d9c
Fernando Garcia <fkngv_10@hotmail.com>
2017-04-11 11:46:49 -0500

    Estilos página de bienvenida

--- templates/bienvenida.php
+++ templates/bienvenida.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Bienvenida</title>
+    <link rel="stylesheet" href="../css/style.css">
     <?php 
         session_start();
         if (!$_SESSION) {
@@ -25,23 +26,23 @@
 <body>
     <section class="wrap">
         <section class="contenido">
-            <h1><?php echo "Bienvenido $nombre"; ?></h1>
+            <h1 class="contenido__title"><?php echo "Bienvenido $nombre"; ?></h1>
             <br>
             <?php if ($_SESSION['tipo'] != 2) { ?>
-                <a href="cliente/form_alta_cliente.php">Registrar cliente</a>
-                <a href="medico/form_alta_medico.php">Registrar médico</a>
-                <a href="mascota/form_alta_mascota.php">Registrar mascota</a>
-                <a href="servicio/form_alta_servicio.php">Registrar servicio</a>
-                <a href="cliente/reporte_clientes.php">Consultar clientes</a>
-                <a href="medico/reporte_medicos.php">Consultar medicos</a>
-                <a href="servicio/reporte_servicios.php">Consultar servicios</a>
+                <a href="cliente/form_alta_cliente.php" class="contenido__link create">Registrar cliente</a>
+                <a href="medico/form_alta_medico.php" class="contenido__link create">Registrar médico</a>
+                <a href="mascota/form_alta_mascota.php" class="contenido__link create">Registrar mascota</a>
+                <a href="servicio/form_alta_servicio.php" class="contenido__link create">Registrar servicio</a>
+                <a href="cliente/reporte_clientes.php" class="contenido__link report">Consultar clientes</a>
+                <a href="medico/reporte_medicos.php" class="contenido__link report">Consultar medicos</a>
+                <a href="servicio/reporte_servicios.php" class="contenido__link report">Consultar servicios</a>
             <?php } else {?>
-                <a href="mascota/reporte_mascotas.php?cliente=<?php echo $_SESSION['nombre']; ?>">Consultar mascotas</a>
-                <a href="#">Agendar cita</a>
-                <a href="#">Consultar citas</a>
+                <a href="mascota/reporte_mascotas.php?cliente=<?php echo $_SESSION['nombre']; ?>" class="contenido__link report">Consultar mascotas</a>
+                <a href="#" class="contenido__link">Agendar cita</a>
+                <a href="#" class="contenido__link report">Consultar citas</a>
             <?php } ?>
             <br><br>
-            <a href="logout.php">Cerrar sesión</a>
+            <a href="logout.php" class="contenido__link logout">Cerrar sesión</a>
         </section>
     </section>
 </body>
