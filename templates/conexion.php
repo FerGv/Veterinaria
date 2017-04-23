0b4bf290c62f5f57cf7d428c5f6959d4d7eb656d
Fernando Garcia <fkngv_10@hotmail.com>
2017-04-23 02:16:46 -0500

    Exclusión de llaves privadas

--- templates/conexion.php
+++ templates/conexion.php
@@ -1,3 +1,7 @@
 <?php
-    $conexion = mysqli_connect("localhost","root","","veterinaria");
+    $DB_HOST = '';
+    $DB_USER = '';
+    $DB_PASS = '';
+    $DB_NAME = '';
+    $conexion = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 ?>
