b70f435b7cbbb06e6ab7501ad8b9824f8da97c26
Fer Gv <fkngv_10@hotmail.com>
2017-06-04 15:59:56 -0500

    Cambiar query de la base de datos

--- templates/conexion.php
+++ templates/conexion.php
@@ -1,7 +1,7 @@
 <?php
-    $DB_HOST = 'localhost';
+    $DB_HOST = '127.0.0.1';
     $DB_USER = 'root';
-    $DB_PASS = '';
+    $DB_PASS = 'root';
     $DB_NAME = 'veterinaria';
     $conexion = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 ?>
--- veterinaria.sql
+++ veterinaria.sql
@@ -1,421 +1,152 @@
--- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 04-05-2017 a las 06:40:38
--- Versión del servidor: 10.1.21-MariaDB
--- Versión de PHP: 7.1.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `veterinaria`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `cita`
---
-
-CREATE TABLE `cita` (
-  `clave_cita` int(11) NOT NULL,
-  `fecha_cita` date NOT NULL,
-  `id_mascota` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Volcado de datos para la tabla `cita`
---
-
-INSERT INTO `cita` (`clave_cita`, `fecha_cita`, `id_mascota`) VALUES
-(3, '2017-05-16', 21),
-(4, '2017-05-31', 20);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `cliente`
---
-
-CREATE TABLE `cliente` (
-  `rfc_cliente` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
-  `nombre_cliente` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `direccion_cliente` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `telefono_cliente` int(11) NOT NULL,
-  `email_cliente` varchar(30) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Volcado de datos para la tabla `cliente`
---
-
-INSERT INTO `cliente` (`rfc_cliente`, `nombre_cliente`, `direccion_cliente`, `telefono_cliente`, `email_cliente`) VALUES
-('1234', 'abc1', 'abc1', 1234, 'fer@correo.com'),
-('dsafsd', 'safsd', 'asafssad', 122312, 'fer@correo.com'),
-('fer123', 'fer', 'calle', 123, 'fer@correo.com'),
-('fer456', 'Fer', 'Calle No. Col. Del. Ciudad CP', 123, 'fer@correo.com');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `control_servicio`
---
-
-CREATE TABLE `control_servicio` (
-  `clave_control_servicio` int(11) NOT NULL,
-  `fecha_control` date NOT NULL,
-  `id_mascota` int(11) NOT NULL,
-  `rfc_medico` varchar(15) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Volcado de datos para la tabla `control_servicio`
---
-
-INSERT INTO `control_servicio` (`clave_control_servicio`, `fecha_control`, `id_mascota`, `rfc_medico`) VALUES
-(1, '2017-04-16', 16, 'medico123'),
-(2, '2017-04-23', 16, 'medico123'),
-(3, '2017-04-30', 16, 'medico123'),
-(4, '2017-04-30', 16, 'medico123'),
-(5, '2017-04-30', 16, 'medico123'),
-(6, '2017-04-30', 16, 'medico123'),
-(7, '2017-04-30', 16, 'medico123'),
-(8, '2017-04-30', 16, 'medico123'),
-(10, '2017-04-30', 16, 'medico123'),
-(11, '2017-04-30', 22, 'medico123'),
-(12, '2017-04-30', 21, 'medico123'),
-(13, '2017-04-30', 23, 'medico123'),
-(14, '2017-05-01', 23, 'medico123'),
-(15, '2017-05-01', 22, 'medico123'),
-(16, '2017-05-01', 21, 'medico123'),
-(17, '2017-05-01', 20, 'medico123');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `control_servicio_servicio`
---
-
-CREATE TABLE `control_servicio_servicio` (
-  `clave_control_servicio` int(11) NOT NULL,
-  `clave_servicio` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Volcado de datos para la tabla `control_servicio_servicio`
---
-
-INSERT INTO `control_servicio_servicio` (`clave_control_servicio`, `clave_servicio`) VALUES
-(1, 6),
-(1, 7),
-(2, 6),
-(3, 6),
-(3, 7),
-(3, 6),
-(11, 6),
-(12, 6),
-(12, 7),
-(13, 6),
-(10, 7),
-(14, 6),
-(15, 6),
-(16, 6),
-(16, 7),
-(17, 6);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `factura`
---
-
-CREATE TABLE `factura` (
-  `clave_factura` int(11) NOT NULL,
-  `fecha_factura` date NOT NULL,
-  `hora_factura` time NOT NULL,
-  `clave_control_servicio` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Volcado de datos para la tabla `factura`
---
-
-INSERT INTO `factura` (`clave_factura`, `fecha_factura`, `hora_factura`, `clave_control_servicio`) VALUES
-(1, '2017-04-23', '08:59:16', 1);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `historial`
---
-
-CREATE TABLE `historial` (
-  `id_mascota` int(11) NOT NULL,
-  `fechaseg_historial` date NOT NULL,
-  `clave_control_servicio` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Volcado de datos para la tabla `historial`
---
-
-INSERT INTO `historial` (`id_mascota`, `fechaseg_historial`, `clave_control_servicio`) VALUES
-(16, '2017-04-30', 2),
-(16, '2017-05-04', 3),
-(16, '2017-05-05', 3),
-(16, '2017-05-10', 10),
-(22, '2017-04-30', 11),
-(21, '2017-04-30', 12),
-(23, '2017-04-30', 13),
-(23, '2017-05-15', 14),
-(22, '2017-05-18', 15),
-(21, '2017-05-19', 16),
-(20, '2017-05-30', 17);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `mascota`
---
-
-CREATE TABLE `mascota` (
-  `id_mascota` int(11) NOT NULL,
-  `nombre_mascota` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `especie_mascota` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `raza_mascota` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `color_mascota` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tamanio_mascota` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-  `seniapart_mascota` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `fechanac_mascota` date NOT NULL,
-  `rfc_cliente` varchar(15) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Volcado de datos para la tabla `mascota`
---
-
-INSERT INTO `mascota` (`id_mascota`, `nombre_mascota`, `especie_mascota`, `raza_mascota`, `color_mascota`, `tamanio_mascota`, `seniapart_mascota`, `fechanac_mascota`, `rfc_cliente`) VALUES
-(15, 'abc1', 'abc1', 'abc1', 'abc1', 'abc1', 'abc1', '2017-04-25', '1234'),
-(16, 'ferchito', 'perro', 'perro', 'cafe', 'grande', 'tatuaje', '2017-01-08', 'fer123'),
-(17, 'admin', 'a', 'a', 'a', 'a', 'a', '2017-04-19', 'fer456'),
-(18, 'asds', 'das', 'dsafa', 'asd', 'sadfds', 'sadfs', '2017-04-24', 'dsafsd'),
-(20, 'ferchito2', 'asd', 'asd', 'asd', 'asd', 'asd', '2017-04-10', 'fer123'),
-(21, 'ferchito3', 'asd', 'asda', 'adqs', 'ssdfasd', 'asda', '2017-04-04', 'fer123'),
-(22, 'ferchito4', 'asd', 'asda', 'adqs', 'ssdfasd', 'asda', '2017-04-04', 'fer123'),
-(23, 'ferchito5', 'ada', 'qqwdsa', 'ds', 'ads', 'ads', '2017-04-18', 'fer123');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `medico`
---
-
-CREATE TABLE `medico` (
-  `rfc_medico` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
-  `nombre_medico` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `direccion_medico` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `telefono_medico` int(11) NOT NULL,
-  `email_medico` varchar(30) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Volcado de datos para la tabla `medico`
---
-
-INSERT INTO `medico` (`rfc_medico`, `nombre_medico`, `direccion_medico`, `telefono_medico`, `email_medico`) VALUES
-('medico123', 'doctor', 'direccion', 12345678, 'medico@email.com');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `servicio`
---
-
-CREATE TABLE `servicio` (
-  `clave_servicio` int(11) NOT NULL,
-  `descripcion_servicio` text COLLATE utf8_unicode_ci NOT NULL,
-  `precio_servicio` float NOT NULL,
-  `tipo_servicio` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
-  `periodicidad_servicio` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Volcado de datos para la tabla `servicio`
---
-
-INSERT INTO `servicio` (`clave_servicio`, `descripcion_servicio`, `precio_servicio`, `tipo_servicio`, `periodicidad_servicio`) VALUES
-(6, 'v1', 1, 'vacuna', '2017-04-27'),
-(7, 'v2', 1, 'vacuna', NULL);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuario`
---
-
-CREATE TABLE `usuario` (
-  `nombre_usuario` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
-  `pass_usuario` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `tipo_usuario` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Volcado de datos para la tabla `usuario`
---
-
-INSERT INTO `usuario` (`nombre_usuario`, `pass_usuario`, `tipo_usuario`) VALUES
-('43543', 'veterinaria123', 2),
-('admin', '123', 0),
-('dsafsd', 'veterinaria123', 2),
-('fer123', 'veterinaria123', 2),
-('fer456', 'veterinaria123', 2),
-('VET001', 'empleado_1', 1);
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `cita`
---
-ALTER TABLE `cita`
-  ADD PRIMARY KEY (`clave_cita`);
-
---
--- Indices de la tabla `cliente`
---
-ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`rfc_cliente`);
-
---
--- Indices de la tabla `control_servicio`
---
-ALTER TABLE `control_servicio`
-  ADD PRIMARY KEY (`clave_control_servicio`),
-  ADD KEY `id_mascota` (`id_mascota`),
-  ADD KEY `rfc_medico` (`rfc_medico`);
-
---
--- Indices de la tabla `control_servicio_servicio`
---
-ALTER TABLE `control_servicio_servicio`
-  ADD KEY `clave_servicio` (`clave_servicio`),
-  ADD KEY `clave_control_servicio` (`clave_control_servicio`);
-
---
--- Indices de la tabla `factura`
---
-ALTER TABLE `factura`
-  ADD PRIMARY KEY (`clave_factura`),
-  ADD KEY `clave_control_servicio` (`clave_control_servicio`);
-
---
--- Indices de la tabla `historial`
---
-ALTER TABLE `historial`
-  ADD KEY `id_mascota` (`id_mascota`),
-  ADD KEY `clave_control_servicio` (`clave_control_servicio`);
-
---
--- Indices de la tabla `mascota`
---
-ALTER TABLE `mascota`
-  ADD PRIMARY KEY (`id_mascota`),
-  ADD KEY `id_usuario` (`rfc_cliente`);
-
---
--- Indices de la tabla `medico`
---
-ALTER TABLE `medico`
-  ADD PRIMARY KEY (`rfc_medico`);
-
---
--- Indices de la tabla `servicio`
---
-ALTER TABLE `servicio`
-  ADD PRIMARY KEY (`clave_servicio`);
-
---
--- Indices de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD UNIQUE KEY `nombre_usuario` (`nombre_usuario`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `cita`
---
-ALTER TABLE `cita`
-  MODIFY `clave_cita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT de la tabla `control_servicio`
---
-ALTER TABLE `control_servicio`
-  MODIFY `clave_control_servicio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
---
--- AUTO_INCREMENT de la tabla `factura`
---
-ALTER TABLE `factura`
-  MODIFY `clave_factura` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT de la tabla `mascota`
---
-ALTER TABLE `mascota`
-  MODIFY `id_mascota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
---
--- AUTO_INCREMENT de la tabla `servicio`
---
-ALTER TABLE `servicio`
-  MODIFY `clave_servicio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `control_servicio`
---
-ALTER TABLE `control_servicio`
-  ADD CONSTRAINT `control_servicio_ibfk_1` FOREIGN KEY (`id_mascota`) REFERENCES `mascota` (`id_mascota`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `control_servicio_ibfk_2` FOREIGN KEY (`rfc_medico`) REFERENCES `medico` (`rfc_medico`) ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `control_servicio_servicio`
---
-ALTER TABLE `control_servicio_servicio`
-  ADD CONSTRAINT `control_servicio_servicio_ibfk_1` FOREIGN KEY (`clave_servicio`) REFERENCES `servicio` (`clave_servicio`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `control_servicio_servicio_ibfk_2` FOREIGN KEY (`clave_control_servicio`) REFERENCES `control_servicio` (`clave_control_servicio`) ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `factura`
---
-ALTER TABLE `factura`
-  ADD CONSTRAINT `factura_ibfk_1` FOREIGN KEY (`clave_control_servicio`) REFERENCES `control_servicio` (`clave_control_servicio`) ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `historial`
---
-ALTER TABLE `historial`
-  ADD CONSTRAINT `historial_ibfk_1` FOREIGN KEY (`id_mascota`) REFERENCES `mascota` (`id_mascota`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `historial_ibfk_2` FOREIGN KEY (`clave_control_servicio`) REFERENCES `control_servicio` (`clave_control_servicio`) ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `mascota`
---
-ALTER TABLE `mascota`
-  ADD CONSTRAINT `mascota_ibfk_1` FOREIGN KEY (`rfc_cliente`) REFERENCES `cliente` (`rfc_cliente`) ON UPDATE CASCADE;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+---------------------------------------------------------------------
+-- Creación de la base de datos
+---------------------------------------------------------------------
+CREATE DATABASE veterinaria;
+
+---------------------------------------------------------------------
+-- Cambiar a la base de datos creada
+---------------------------------------------------------------------
+USE veterinaria;
+
+---------------------------------------------------------------------
+-- Creación de tablas
+---------------------------------------------------------------------
+CREATE TABLE cita (
+  clave_cita int NOT NULL AUTO_INCREMENT,
+  fecha_cita date NOT NULL,
+  id_mascota int NOT NULL,
+  PRIMARY KEY (clave_cita)
+) ENGINE=InnoDB;
+
+CREATE TABLE cliente (
+  rfc_cliente varchar(15) NOT NULL,
+  nombre_cliente varchar(50) NOT NULL,
+  direccion_cliente varchar(50) NOT NULL,
+  telefono_cliente int NOT NULL,
+  email_cliente varchar(30) NOT NULL,
+  PRIMARY KEY (rfc_cliente)
+) ENGINE=InnoDB;
+
+CREATE TABLE control_servicio (
+  clave_control_servicio int NOT NULL AUTO_INCREMENT,
+  fecha_control date NOT NULL,
+  id_mascota int NOT NULL,
+  rfc_medico varchar(15) NOT NULL,
+  PRIMARY KEY (clave_control_servicio)
+) ENGINE=InnoDB;
+
+CREATE TABLE control_servicio_servicio (
+  clave_control_servicio int NOT NULL,
+  clave_servicio int NOT NULL
+) ENGINE=InnoDB;
+
+CREATE TABLE factura (
+  clave_factura int NOT NULL AUTO_INCREMENT,
+  fecha_factura date NOT NULL,
+  hora_factura time NOT NULL,
+  clave_control_servicio int NOT NULL,
+  PRIMARY KEY (clave_factura)
+) ENGINE=InnoDB;
+
+CREATE TABLE historial (
+  id_mascota int NOT NULL,
+  fechaseg_historial date NOT NULL,
+  clave_control_servicio int NOT NULL
+) ENGINE=InnoDB;
+
+CREATE TABLE mascota (
+  id_mascota int NOT NULL AUTO_INCREMENT,
+  nombre_mascota varchar(20) NOT NULL,
+  especie_mascota varchar(20) NOT NULL,
+  raza_mascota varchar(20) NOT NULL,
+  color_mascota varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  tamanio_mascota varchar(10) NOT NULL,
+  seniapart_mascota varchar(50) NOT NULL,
+  fechanac_mascota date NOT NULL,
+  rfc_cliente varchar(15) NOT NULL,
+  PRIMARY KEY (id_mascota)
+) ENGINE=InnoDB;
+
+CREATE TABLE medico (
+  rfc_medico varchar(15) NOT NULL,
+  nombre_medico varchar(50) NOT NULL,
+  direccion_medico varchar(50) NOT NULL,
+  telefono_medico int(11) NOT NULL,
+  email_medico varchar(30) NOT NULL,
+  PRIMARY KEY (rfc_medico)
+) ENGINE=InnoDB;
+
+CREATE TABLE servicio (
+  clave_servicio int NOT NULL AUTO_INCREMENT,
+  descripcion_servicio text NOT NULL,
+  precio_servicio float NOT NULL,
+  tipo_servicio varchar(15) NOT NULL,
+  periodicidad_servicio date DEFAULT NULL,
+  PRIMARY KEY (clave_servicio)
+) ENGINE=InnoDB;
+
+CREATE TABLE usuario (
+  nombre_usuario varchar(15) NOT NULL,
+  pass_usuario varchar(20) NOT NULL,
+  tipo_usuario int NOT NULL,
+  PRIMARY KEY (nombre_usuario)
+) ENGINE=InnoDB;
+
+---------------------------------------------------------------------
+-- Creación de llaves foráneas
+---------------------------------------------------------------------
+ALTER TABLE control_servicio
+  ADD FOREIGN KEY (id_mascota) REFERENCES mascota (id_mascota) ON UPDATE CASCADE,
+  ADD FOREIGN KEY (rfc_medico) REFERENCES medico (rfc_medico) ON UPDATE CASCADE;
+
+ALTER TABLE control_servicio_servicio
+  ADD FOREIGN KEY (clave_servicio) REFERENCES servicio (clave_servicio) ON UPDATE CASCADE,
+  ADD FOREIGN KEY (clave_control_servicio) REFERENCES control_servicio (clave_control_servicio) ON UPDATE CASCADE;
+
+ALTER TABLE factura
+  ADD FOREIGN KEY (clave_control_servicio) REFERENCES control_servicio (clave_control_servicio) ON UPDATE CASCADE;
+
+ALTER TABLE historial
+  ADD FOREIGN KEY (id_mascota) REFERENCES mascota (id_mascota) ON UPDATE CASCADE,
+  ADD FOREIGN KEY (clave_control_servicio) REFERENCES control_servicio (clave_control_servicio) ON UPDATE CASCADE;
+
+ALTER TABLE mascota
+  ADD FOREIGN KEY (rfc_cliente) REFERENCES cliente (rfc_cliente) ON UPDATE CASCADE;
+  
+---------------------------------------------------------------------
+-- Inserción de datos
+---------------------------------------------------------------------
+INSERT INTO usuario VALUES ('admin', '123', 0);
+
+---------------------------------------------------------------------
+-- Mostrar todas las tablas
+---------------------------------------------------------------------
+SHOW TABLES;
+
+---------------------------------------------------------------------
+-- Mostrar todos los registros de las tablas
+---------------------------------------------------------------------
+SELECT * FROM cita;
+SELECT * FROM cliente;
+SELECT * FROM control_servicio;
+SELECT * FROM control_servicio_servicio;
+SELECT * FROM factura;
+SELECT * FROM historial;
+SELECT * FROM mascota;
+SELECT * FROM medico;
+SELECT * FROM servicio;
+SELECT * FROM usuario;
+
+---------------------------------------------------------------------
+-- Mostrar la estructura de las tablas
+---------------------------------------------------------------------
+SHOW COLUMNS FROM cita; 						-- DESCRIBE cita;
+SHOW COLUMNS FROM cliente; 						-- DESCRIBE cliente;
+SHOW COLUMNS FROM control_servicio; 			-- DESCRIBE control_servicio;
+SHOW COLUMNS FROM control_servicio_servicio; 	-- DESCRIBE control_servicio_servicio;
+SHOW COLUMNS FROM factura; 						-- DESCRIBE factura;
+SHOW COLUMNS FROM historial; 					-- DESCRIBE historial;
+SHOW COLUMNS FROM mascota; 						-- DESCRIBE mascota;
+SHOW COLUMNS FROM medico;					 	-- DESCRIBE medico;
+SHOW COLUMNS FROM servicio; 					-- DESCRIBE servicio;
+SHOW COLUMNS FROM usuario; 						-- DESCRIBE usuario;
